47c8198c7e7a8cc8542463294c67cadd39dddaaa
Mansi Shah <91773023+Mansi21Shah@users.noreply.github.com>
2022-02-02 12:41:19 +0530

    2nd Commit

--- SimplePrograms/src/simplePrograms/Addition.java
+++ SimplePrograms/src/simplePrograms/Addition.java
@@ -1,9 +1,18 @@
 package simplePrograms;
+import java.util.*;
 
 public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a,b,c;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter 1st Number: ");
+		a= scan.nextInt();
+		System.out.println("Enter 2nd Number: ");
+		b= scan.nextInt();
+		c= a+b;
+		System.out.println("Result = " + c);
 	}
 
 }
