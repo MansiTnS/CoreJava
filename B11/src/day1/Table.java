4961d4f40639322d7094cd13772b81dbf5c55424
Mansi Shah <91773023+Mansi21Shah@users.noreply.github.com>
2022-02-28 18:31:20 +0530

    1st Commit

---
+++ B11/src/day1/Table.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10, b=20,c;
+		c= a+b;
+		System.out.println("Result:" + c);
+		
+
+	}
+
+}
